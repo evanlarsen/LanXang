b4a8cd4a16f28f341167615632c07c2ba5928246
Evan Larsen <evan.larsen@gmail.com>
2012-06-28 20:35:50 -0400

    initial commit

---
+++ LanXang.Web/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LanXang.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult DinnerMenu()
+        {
+            return View();
+        }
+
+        public ActionResult LunchMenu()
+        {
+            return View();
+        }
+
+        public ActionResult SushiMenu()
+        {
+            return View();
+        }
+
+        public ActionResult FeaturedSushi()
+        {
+            return View();
+        }
+
+        public ActionResult Location()
+        {
+            return View();
+        }
+
+        public ActionResult Delivery()
+        {
+            return View();
+        }
+    }
+}
---
+++ LanXang.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LanXang.Web.MvcApplication" Language="C#" %>
