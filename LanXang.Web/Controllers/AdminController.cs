b5f691c1cf2a53a335228b3bad336158b135f19a
Evan Larsen <evan.larsen@gmail.com>
2012-06-29 19:13:38 -0400

    adding admin section

---
+++ LanXang.Web/Controllers/AdminController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace LanXang.Web.Controllers
+{
+    public class AdminController : Controller
+    {
+        //
+        // GET: /Admin/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string password)
+        {
+            if (password == "letmein")
+            {
+                FormsAuthentication.SetAuthCookie("admin", false);
+                return RedirectToAction("MainMenu");
+            }
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult MainMenu()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult DinnerMenu()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult SushiMenu()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult StoreHours()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult Gallery()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult Email()
+        {
+            return View();
+        }
+    }
+}
---
+++ LanXang.Web/Viewmodels/AdminLoginVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LanXang.Web.Viewmodels
+{
+    public class AdminLoginVM
+    {
+        public string Password { get; set; }
+        public bool LoginFailed { get; set; }
+    }
+}
