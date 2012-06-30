75f01f18d3b6851b5e19db9b8e758674e4f63e11
Evan Larsen <evan.larsen@gmail.com>
2012-06-29 20:25:34 -0400

    working on store hours

--- LanXang.Web/Controllers/AdminController.cs
+++ LanXang.Web/Controllers/AdminController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Security;
+using LanXang.Web.Viewmodels;
 
 namespace LanXang.Web.Controllers
 {
@@ -49,7 +50,20 @@ namespace LanXang.Web.Controllers
         [Authorize]
         public ActionResult StoreHours()
         {
-            return View();
+            StoreHoursVM vm = new StoreHoursVM()
+            {
+                Line1 = "Mon-Thurs: 11am-10pm",
+                Line2 = "Friday: 11am-10:30pm",
+                Line3 = "Saturday: 12-10:30pm"
+            };
+            return View(vm);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult StoreHours(StoreHoursVM vm)
+        {
+            return View(vm);
         }
 
         [Authorize]
---
+++ LanXang.Web/Viewmodels/StoreHoursVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LanXang.Web.Viewmodels
+{
+    public class StoreHoursVM
+    {
+        public string Line1 { get; set; }
+        public string Line2 { get; set; }
+        public string Line3 { get; set; }
+    }
+}
